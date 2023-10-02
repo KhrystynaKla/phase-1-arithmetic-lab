c5151082b4be134cce37122d337a7a414500ebe1
Khrystyna Klapushchak <klapushchak.khr@gmail.com>
2023-10-02 00:02:19 -0400

    lab competed

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 31;
+let num2 = 2;
+let num3=20;
+let num4 = 8
+let multiply = num1*num2;
+let random = Math.floor(Math.random()*10);
+let mod = num3%num4;
+let max = Math.max(num2, num3,num4);
 // Write your code here
